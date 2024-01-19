8b545e4ec0722cf408e40ef8b1534fb5801dd049
Higunio320 <wojtek123713@gmail.com>
2024-01-19 11:58:56 +0100

    Added https connection for backend

--- bank-backend/src/main/java/com/bank/BankApplication.java
+++ bank-backend/src/main/java/com/bank/BankApplication.java
@@ -1,12 +1,14 @@
 package com.bank;
 
 import com.bank.api.auth.interfaces.AuthService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@Slf4j
 public class BankApplication {
 
 	public static void main(String[] args) {
@@ -19,9 +21,11 @@ public class BankApplication {
 	}
 
 	private static void registerExampleUser(AuthService authService) {
+		log.info("Registering user");
 		String username = "user123";
 		String password = "abcdefghijklmnoprsuwxyz";
 		authService.register(username, password);
+		log.info("User registered");
 	}
 
 }
